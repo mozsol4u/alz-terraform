cb90ce4123099713c2bc34acc6a14dac1e119ce1
Omer Zubair <omerzubair.ae@gmail.com>
2026-01-31 01:35:24 -0500

    cd1

--- modules/management-groups/main.tf
+++ modules/management-groups/main.tf
@@ -1,8 +1,5 @@
-
-// modules/management-groups/main.tf
 module "mg" {
   source = "git::https://github.com/Azure/avm-res-managementgroup-azurerm.git?ref=v0.4"
-  version = "~> 0.4"
 
   root_management_group_name = var.root_management_group_name
   management_groups          = var.management_groups
