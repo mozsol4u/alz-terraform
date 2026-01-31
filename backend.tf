0c4145a5fc6196d5ab212c9f05efc5c41a281425
Omer Zubair <omerzubair.ae@gmail.com>
2026-01-31 01:42:37 -0500

    ds

--- backend.tf
+++ backend.tf
@@ -2,9 +2,9 @@
 // ---------- backend.tf ----------
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.tfstate_rg
-    storage_account_name = var.tfstate_sa
-    container_name       = var.tfstate_container
+    // resource_group_name  = var.tfstate_rg
+    // storage_account_name = var.tfstate_sa
+    // container_name       = var.tfstate_container
     key                  = "alz.tfstate"
     use_azuread_auth     = true
   }
