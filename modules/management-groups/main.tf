86c45c92b63f33bfacfe75ce1aaaad4fd9f32720
Omer Zubair <omerzubair.ae@gmail.com>
2026-01-31 01:29:14 -0500

    cv

--- modules/management-groups/main.tf
+++ modules/management-groups/main.tf
@@ -1,7 +1,7 @@
 
 // modules/management-groups/main.tf
 module "mg" {
-  source = "Azure/avm-res-management-managementgroup/azurerm"
+  source = "Azure/avm-res-managementgroup/azurerm"
   version = "~> 0.4"
 
   root_management_group_name = var.root_management_group_name
